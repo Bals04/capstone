f7f7ed0f9aea0e74d9602132313bc7521ea82b32
Bals <balsamojj04@gmail.com>
2024-07-24 15:31:58 +0800

    updates for the button navigation

--- server/server.js
+++ server/server.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/views/features/member.html'));
 });
+app.get('/mealsPage', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/views/features/meals.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
