809a4c8164e2d615669cdf33e5109d825a5f1141
Bals <balsamojj04@gmail.com>
2024-08-19 17:04:51 +0800

    modified the tailwind config and landingpage colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,13 +6,18 @@ module.exports = {
     "./views/admin/**/*.{html,js}",
     "./views/maps/**/*.{html,js}",
     "./views/features/**/*.{html,js}",
+    "./views/Landing Page/**/*.{html,js}",
     "./scripts/map.js",
-    "./views/Landing Page/**/*.{html,js}"
   ],
   theme: {
-
-    colors: {
-      primary: {
+    extend: {
+      fontFamily:{
+        customFont: ['"Aldrich"', "sans-serif"],
+        customFont2:['"Montserrat"', "sans-serif"]
+      },
+      colors: {
+        customGray: '#343639',
+        customOrange: '#EC7E4A',
         50: '#EC7E4A',
         51: '#343639',
         52: '#FFFFFF',
@@ -23,16 +28,6 @@ module.exports = {
         guestNavbar: '#232121',
         forGradientGray: '#232323',
         'forGradientWhite': '#898787',
-      }
-    },
-    extend: {
-      fontFamily:{
-        customFont: ['"Aldrich"', "sans-serif"],
-        customFont2:['"Montserrat"', "sans-serif"]
-      },
-      colors: {
-        customGray: '#343639',
-        customOrange: '#EC7E4A',
     },},
   },
   plugins: [],
