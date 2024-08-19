9bd8364cb45b097f0bae1b9665531a9b8920c5b5
Bals <balsamojj04@gmail.com>
2024-08-19 18:45:25 +0800

    latest commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,10 +8,9 @@ module.exports = {
     "./views/features/**/*.{html,js}",
     "./views/Landing Page/**/*.{html,js}",
     "./scripts/map.js",
-    
+
   ],
   theme: {
-<<<<<<< HEAD
 
     colors: {
       50: '#EC7E4A',
@@ -25,31 +24,12 @@ module.exports = {
       customGray: '#343639',
       customOrange: '#EC7E4A',
     },
-=======
->>>>>>> 809a4c8164e2d615669cdf33e5109d825a5f1141
     extend: {
       fontFamily:{
         customFont: ['"Aldrich"', "sans-serif"],
         customFont2:['"Montserrat"', "sans-serif"]
       },
-<<<<<<< HEAD
     },
-=======
-      colors: {
-        customGray: '#343639',
-        customOrange: '#EC7E4A',
-        50: '#EC7E4A',
-        51: '#343639',
-        52: '#FFFFFF',
-        'custom-bg': '#323538',
-        'custom-border': '#EC7E4A',
-        'customGray': '#343639',
-        customOrange: '#EC7E4A',
-        guestNavbar: '#232121',
-        forGradientGray: '#232323',
-        'forGradientWhite': '#898787',
-    },},
->>>>>>> 809a4c8164e2d615669cdf33e5109d825a5f1141
   },
   plugins: [],
 }
