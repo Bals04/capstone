03e2b683936e9c53c10f04a9105cd9e4afc8bc8d
Atlas Fox <charlescuizon2003@gmail.com>
2024-08-19 13:27:48 +0800

    Aug 19

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,10 +6,30 @@ module.exports = {
     "./views/admin/**/*.{html,js}",
     "./views/maps/**/*.{html,js}",
     "./views/features/**/*.{html,js}",
-    "./scripts/map.js"
+    "./scripts/map.js",
+    "./views/Landing Page/**/*.{html,js}"
   ],
   theme: {
+
+    colors: {
+      primary: {
+        50: '#EC7E4A',
+        51: '#343639',
+        52: '#FFFFFF',
+        'custom-bg': '#323538',
+        'custom-border': '#EC7E4A',
+        'customGray': '#343639',
+        customOrange: '#EC7E4A',
+        guestNavbar: '#232121',
+        forGradientGray: '#232323',
+        'forGradientWhite': '#898787',
+      }
+    },
     extend: {
+      fontFamily:{
+        customFont: ['"Aldrich"', "sans-serif"],
+        customFont2:['"Montserrat"', "sans-serif"]
+      },
       colors: {
         customGray: '#343639',
         customOrange: '#EC7E4A',
