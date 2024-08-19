74ed3020f5083ae47b8a232874f9628682b72a02
Atlas Fox <charlescuizon2003@gmail.com>
2024-08-19 18:13:39 +0800

    Aug 19 6:13pm

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,28 +12,23 @@ module.exports = {
   theme: {
 
     colors: {
-      primary: {
-        50: '#EC7E4A',
-        51: '#343639',
-        52: '#FFFFFF',
-        'custom-bg': '#323538',
-        'custom-border': '#EC7E4A',
-        'customGray': '#343639',
-        customOrange: '#EC7E4A',
-        guestNavbar: '#232121',
-        forGradientGray: '#232323',
-        'forGradientWhite': '#898787',
-      }
+      50: '#EC7E4A',
+      51: '#343639',
+      52: '#FFFFFF',
+      'custom-bg': '#323538',
+      'custom-border': '#EC7E4A',
+      guestNavbar: '#232121',
+      forGradientGray: '#232323',
+      'forGradientWhite': '#898787',
+      customGray: '#343639',
+      customOrange: '#EC7E4A',
     },
     extend: {
       fontFamily:{
         customFont: ['"Aldrich"', "sans-serif"],
         customFont2:['"Montserrat"', "sans-serif"]
       },
-      colors: {
-        customGray: '#343639',
-        customOrange: '#EC7E4A',
-    },},
+    },
   },
   plugins: [],
 }
