75d31b75793d5d0517a8558c7458ae595d3fca6a
Bals <balsamojj04@gmail.com>
2024-08-19 19:09:44 +0800

    fixed the color issue between landingpage.html and maps.html

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,23 +11,22 @@ module.exports = {
 
   ],
   theme: {
-
-    colors: {
-      50: '#EC7E4A',
-      51: '#343639',
-      52: '#FFFFFF',
-      'custom-bg': '#323538',
-      'custom-border': '#EC7E4A',
-      guestNavbar: '#232121',
-      forGradientGray: '#232323',
-      'forGradientWhite': '#898787',
-      customGray: '#343639',
-      customOrange: '#EC7E4A',
-    },
     extend: {
       fontFamily:{
         customFont: ['"Aldrich"', "sans-serif"],
         customFont2:['"Montserrat"', "sans-serif"]
+      },
+      colors: {
+        50: '#EC7E4A',
+        51: '#343639',
+        52: '#FFFFFF',
+        'custom-bg': '#323538',
+        'custom-border': '#EC7E4A',
+        guestNavbar: '#232121',
+        forGradientGray: '#232323',
+        'forGradientWhite': '#898787',
+        customGray: '#343639',
+        customOrange: '#EC7E4A',
       },
     },
   },
