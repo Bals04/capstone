65afaf35c099077fd2fc4b7e7cf55052c36a0451
Atlas Fox <charlescuizon2003@gmail.com>
2024-08-28 14:35:51 +0800

    Aug 28

--- frontend/scripts/registration.js
+++ frontend/scripts/registration.js
@@ -1,14 +1,14 @@
-function view_Pending(){
-    var view_pending = document.getElementById('view_pending')
-    view_pending.classList.remove('invisible')
+function view_Permit(){
+    var view_permit = document.getElementById('view_permit')
+    view_permit.classList.remove('hidden')
 
     var content_container = document.getElementById('content_container')
     content_container.classList.add('blur-lg')
 }
 
-function close_Pending(){
-    var close_pending = document.getElementById('view_pending')
-    close_pending.classList.add('invisible')
+function close_Img(){
+    var close_permit = document.getElementById('view_permit')
+    close_permit.classList.add('hidden')
 
     var content_container = document.getElementById('content_container')
     content_container.classList.remove('blur-lg')
