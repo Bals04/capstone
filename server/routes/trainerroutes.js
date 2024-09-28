3b7e6dc50c711ad090c21193bfc09861956f1bc4
Bals <balsamojj04@gmail.com>
2024-09-28 16:17:44 +0800

    validated the inputs of the trainer

--- server/routes/trainerroutes.js
+++ server/routes/trainerroutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const trainerController = require('../controllers/trainerController');
 const validation = require('../middlewares/validation')
 const trainerSchema = require('../validations/trainerValidations')
+const templateSchema = require('../validations/templateValidations')
+const exerciseSchema = require('../validations/templateExercisesValidations')
 
 router.get("/getTrainers", trainerController.getAllTrainers); 
 router.get("/getTrainerInfo", trainerController.getTrainerInfo); 
@@ -12,8 +14,8 @@ router.get("/retrieveMemberChatLog", trainerController.retrieveMemberChatLog);
 router.get("/getTemplates", trainerController.getTemplates); 
 router.get("/getTemplateId", trainerController.getTemplateId); 
 router.post("/createGymTrainer",validation(trainerSchema), trainerController.createGymTrainer); 
-router.post("/createWorkoutTemplate", trainerController.insertWorkoutTemplate); 
-router.post("/createWorkoutTemplateExercises", trainerController.insertWorkoutTemplateExercise); 
+router.post("/createWorkoutTemplate",validation(templateSchema), trainerController.insertWorkoutTemplate); 
+router.post("/createWorkoutTemplateExercises",validation(exerciseSchema), trainerController.insertWorkoutTemplateExercise); 
 router.put("/ModifyWorkoutTemplateExercises", trainerController.updateWorkoutTemplateExercise); 
 router.delete("/RemoveWorkoutTemplateExercises", trainerController.removeTemplateExercise); 
 
